c90bcca187e2488a223e1c257cc62474c18d351e
Administrator <Administrator@Khagen.connect>
2020-06-06 03:10:43 +0100

    Firat commit..

---
+++ src/com/github/project/Test1.java
@@ -0,0 +1,14 @@
+package com.github.project;
+
+public class Test1 {
+	
+public static void main(String[] args) {
+	String hungry ="yes";
+	if(hungry=="yes") {
+		System.out.println("Eat food");
+		}
+	else {
+		System.out.println("Workout more");
+	}
+}
+}
